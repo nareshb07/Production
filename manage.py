7f8926c6f54694be6c7da901d3b1004817a286fa
nareshb07 <k.nareshb0077@gmail.com>
2023-11-02 15:33:16 +0530

    main

--- manage.py
+++ manage.py
@@ -3,9 +3,14 @@
 import os
 import sys
 
+import django
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
+    
+
+    django.setup()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
