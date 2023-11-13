ae74922f71de0fa9d3539c29e5a65d1c6b1ff739
nareshb07 <k.nareshb0077@gmail.com>
2023-11-13 23:50:21 +0530

    main

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'zl4hyafu=ky(m@r&0pbg7qfsro*@r=2r%-js7y3#pj0g8onwvi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['server2-dev.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 16 # http://127.0.0.1:8000
 
