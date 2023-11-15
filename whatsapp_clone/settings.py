1cd9306dc47428b6a20d5bca839afe2f32567b14
nareshb07 <k.nareshb0077@gmail.com>
2023-11-15 13:10:30 +0530

    main

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -136,14 +136,24 @@ ASGI_APPLICATION = 'whatsapp_clone.asgi.application'
 #######3Production#################
 
 
-CHANNEL_LAYERS = {
+CACHES = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('serverredis.m9kdid.ng.0001.aps1.cache.amazonaws.com', 6379)],
-        },
-    },
+        'BACKEND': 'django.core.cache.backends.redis',
+        'LOCATION': 'serverredis.m9kdid.ng.0001.aps1.cache.amazonaws.com:6379',  # Update with your ElastiCache details
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
 }
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('serverredis.m9kdid.ng.0001.aps1.cache.amazonaws.com', 6379)],
+#         },
+#     },
+# }
 
 
 # CACHES = {
